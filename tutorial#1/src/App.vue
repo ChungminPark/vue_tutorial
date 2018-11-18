8b58fe4cdd391cb1bba07c7a611578b639d7df7b
Chungmin Park(Chuck) <chuck.m.park@gmail.com>
2018-11-18 20:12:41 +0900

    first tutorial

---
+++ tutorial#1/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    Parent counter: {{count}} <br>
+    <button @click="addCounter">+</button>
+    <button @click="subCounter">+</button>
+
+    <child v-bind:passedCounter="counter"></child>
+  </div>
+</template>
+
+<script>
+import Child from './Child.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    addCounter() {
+      this.counter++;
+    },
+    subCounter() {
+      this.counter--;
+    }
+  },
+  components: {
+    'child': Child
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
