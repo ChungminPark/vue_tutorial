56a8febb17c5b0bee301a44a122cd9a2dc35c6c6
Chungmin Park(Chuck) <chuck.m.park@gmail.com>
2018-11-18 22:25:52 +0900

    add tutorial#6

---
+++ tutorial#6/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    Parent counter: {{ getCounter }} <br>
+    <button @click="addCounter">+</button>
+    <button @click="subCounter">-</button>
+
+    <child></child>
+  </div>
+</template>
+
+<script>
+import Child from './Child.vue'
+import { mapGetters, mapMutations } from 'vuex'
+
+export default {
+  name: 'app',
+  computed: {
+    ...mapGetters([
+      'getCounter',
+    ]),
+  },
+  methods: {
+    ...mapMutations([
+      'addCounter',
+      'subCounter',
+    ])
+  },
+  components: {
+    'child': Child
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
